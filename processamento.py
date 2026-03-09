f536ac33b0f3ff5dcb3e26699816a5985200c096
DafniMilla <dafni8663@gmail.com>
2026-03-08 22:32:23 -0300

    add estilo

--- processamento.py
+++ processamento.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set_theme(style="whitegrid", context="talk")
+sns.set_theme(style="whitegrid", context="talk",  palette="colorblind")
 
 historico_notas = []
 
@@ -141,6 +141,17 @@ def processar_arquivo(caminho_csv):
          "ano": ano_ref,
          "media_nota": media_notacorte
 })
+    
+    fluxo = (
+         microdados
+    .groupby(["municipio_candidato","municipio_campus"])
+    .size()
+    .reset_index(name="qtd")
+    .sort_values("qtd", ascending=False)
+    .head(10)
+)
+
+
 # SALVAR CSV
 
     BASE_DIR = Path(__file__).resolve().parent
@@ -151,9 +162,8 @@ def processar_arquivo(caminho_csv):
 
     microdados.to_csv(caminho_csv_saida, index=False)
 
-# =========================
+
 # GRÁFICOS
-# =========================
 
 # GÊNERO POR CURSO
 
@@ -164,42 +174,43 @@ def processar_arquivo(caminho_csv):
         hue="tp_sexo",
         palette={"Masculino":"#297BFF","Feminino":"#FF3EA8"}
     )
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=30)
     plt.title("Gênero por Curso")
     img_genero = gerar_grafico(fig)
 
 # GÊNERO GERAL
 
     fig = plt.figure(figsize=(7,5))
-    sns.countplot(data=microdados, x="tp_sexo")
+    sns.countplot(data=microdados, x="tp_sexo", palette="Set1")
     plt.title("Distribuição Geral por Gênero")
     img_genero_geral = gerar_grafico(fig)
 
 # MODALIDADE
 
     fig = plt.figure(figsize=(8,5))
-    sns.countplot(data=microdados, x="tp_modalidade")
+    sns.countplot(data=microdados, x="tp_modalidade", palette="Set2")
     plt.title("Aprovados por Modalidade")
     img_modalidade = gerar_grafico(fig)
 
 # IDADE
 
     fig = plt.figure(figsize=(10,5))
-    sns.histplot(microdados["idade"], bins=25, kde=True)
+    sns.histplot(microdados["idade"], bins=25, kde=True, color="#001F52")
     plt.title("Distribuição Idade")
     img_idade = gerar_grafico(fig)
 
 # IDADE POR CURSO
 
     fig = plt.figure(figsize=(12,7))
-    sns.boxplot(data=microdados, x="idade", y="nome_curso")
+    sns.boxplot(data=microdados, x="idade", y="nome_curso", palette="pastel"
+)
     plt.title("Idade por Curso")
     img_idade_curso = gerar_grafico(fig)
 
 # TURNOS
 
     fig = plt.figure(figsize=(8,5))
-    sns.countplot(data=microdados, x="turno")
+    sns.countplot(data=microdados, x="turno", palette="muted")
     plt.title("Turnos Escolhidos")
     img_turno = gerar_grafico(fig)
 
@@ -209,7 +220,7 @@ def processar_arquivo(caminho_csv):
     topMun.columns = ["Municipio","Qtd"]
 
     fig = plt.figure(figsize=(12,6))
-    sns.barplot(data=topMun, x="Qtd", y="Municipio")
+    sns.barplot(data=topMun, x="Qtd", y="Municipio", palette="viridis")
     plt.title("Top Municípios Campus")
     img_mun = gerar_grafico(fig)
 
@@ -218,7 +229,7 @@ def processar_arquivo(caminho_csv):
     rankingUF = microdados.groupby("uf_campus")["nu_notacorte"].mean().reset_index()
 
     fig = plt.figure(figsize=(10,5))
-    sns.barplot(data=rankingUF, x="uf_campus", y="nu_notacorte")
+    sns.barplot(data=rankingUF, x="uf_campus", y="nu_notacorte", color="#e6fd65")
     plt.title("Nota Média por UF")
     img_uf = gerar_grafico(fig)
 
@@ -234,10 +245,50 @@ def processar_arquivo(caminho_csv):
     )
 
     fig = plt.figure(figsize=(12,6))
-    sns.barplot(data=ranking, x="nu_notacorte", y="nome_curso")
+    sns.barplot(data=ranking, x="nu_notacorte", y="nome_curso", palette="viridis")
     plt.title("Ranking Média Nota")
     img_rank = gerar_grafico(fig)
 
+    # FLUXO ORIGEM -> DESTINO
+
+    fluxo["fluxo"] = fluxo["municipio_candidato"] + " → " + fluxo["municipio_campus"]
+
+    fig = plt.figure(figsize=(12,6))
+
+    sns.barplot(
+        data=fluxo,
+        x="qtd",
+        y="fluxo", 
+        color="#ff7e33"
+)
+
+    plt.title("Principais Fluxos de Origem → Destino")
+    plt.xlabel("Quantidade de Alunos")
+    plt.ylabel("Fluxo")
+
+    img_fluxo = gerar_grafico(fig)
+
+
+    origem = (
+        microdados["municipio_candidato"]
+    .value_counts()
+    .head(10)
+    .reset_index()
+)
+
+    origem.columns = ["Municipio","Qtd"]
+
+    fig = plt.figure(figsize=(12,6))
+
+    sns.barplot(
+        data=origem,
+        x="Qtd",
+        y="Municipio"
+)
+
+    plt.title("Principais Municípios de Origem dos Candidatos"  )
+
+    img_origem = gerar_grafico(fig)
 
 # HTML 
 
@@ -262,32 +313,38 @@ img{{width:100%;margin-bottom:40px}}
 <p>Mediana: {mediana_notacorte:.2f}</p>
 <p>Média Idade: {media_idade:.1f}</p>
 
-<h2>Genero por Curso</h2>
+<h2>Distribuição de Gênero por Curso</h2>
 <img src="data:image/png;base64,{img_genero}"/>
 
-<h2>Genero Geral</h2>
+<h2>Distribuição Geral de Gênero entre os Aprovados</h2>
 <img src="data:image/png;base64,{img_genero_geral}"/>
 
-<h2>Modalidade</h2>
+<h2>Aprovados por Modalidade de Concorrência</h2>
 <img src="data:image/png;base64,{img_modalidade}"/>
 
-<h2>Distribuição Idade</h2>
+<h2>Distribuição da Idade dos Estudantes Aprovados</h2>
 <img src="data:image/png;base64,{img_idade}"/>
 
-<h2>Idade por Curso</h2>
+<h2>Distribuição da Idade por Curso</h2>
 <img src="data:image/png;base64,{img_idade_curso}"/>
 
-<h2>Turnos</h2>
+<h2>Distribuição de Matrículas por Turno</h2>
 <img src="data:image/png;base64,{img_turno}"/>
 
-<h2>Municípios</h2>
+<h2>Principais Municípios com Oferta de Cursos</h2>
 <img src="data:image/png;base64,{img_mun}"/>
 
-<h2>Nota Média UF</h2>
+<h2>Média da Nota de Corte por Unidade Federativa</h2>
 <img src="data:image/png;base64,{img_uf}"/>
 
-<h2>Ranking Nota</h2>
+<h2>Ranking dos Cursos mais Concorridos</h2>
 <img src="data:image/png;base64,{img_rank}"/>
+
+<h2>Principais Fluxos de Origem e Destino dos Estudantes</h2>
+<img src="data:image/png;base64,{img_fluxo}"/>
+
+<h2>Municípios de Origem dos Candidatos</h2>
+<img src="data:image/png;base64,{img_origem}"/>
 
 </body>
 </html>
@@ -376,7 +433,7 @@ img{{width:100%}}
 </head>
 <body>
 
-<h1>Evolução das Notas ao Longo dos Períodos</h1>
+<h1>Evolução das Notas ao Longo dos Anos</h1>
 
 <img src="data:image/png;base64,{img}"/>
 
